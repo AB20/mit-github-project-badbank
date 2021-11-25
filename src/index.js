85cfa93f9954535df77a109d2436124f92607985
AB1 <ABarnes204@aol.com>
2021-11-25 14:29:37 -0500

    update paths

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
+import React from "react";
+import ReactDom from "react-dom";
+
 function Spa() {
   return (
     <HashRouter>
       <NavBar/>
       <UserContext.Provider value={{users:[{name:'abel',email:'abel@mit.edu',password:'secret',balance:100}]}}>
         <div className="container" style={{padding: "20px"}}>
-          <Route path="/" exact component={Home} />
-          <Route path="/CreateAccount/" component={CreateAccount} />
-          <Route path="/login/" component={Login} />
-          <Route path="/deposit/" component={Deposit} />
-          <Route path="/withdraw/" component={Withdraw} />
-          <Route path="/balance/" component={Balance} />
-          <Route path="/alldata/" component={AllData} />
+          <Route path="/components/" exact component={Home} />
+          <Route path="/components/CreateAccount/" component={CreateAccount} />
+          <Route path="/components/login/" component={Login} />
+          <Route path="/components/deposit/" component={Deposit} />
+          <Route path="/components/withdraw/" component={Withdraw} />
+          <Route path="/components/balance/" component={Balance} />
+          <Route path="/components/alldata/" component={AllData} />
         </div>
       </UserContext.Provider>      
     </HashRouter>
