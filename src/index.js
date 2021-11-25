9d7f1406869fe913a5f1fb32a714884bafc4d9d1
AB1 <ABarnes204@aol.com>
2021-11-25 14:32:27 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom";
 
 function Spa() {
   return (
@@ -7,13 +7,13 @@ function Spa() {
       <NavBar/>
       <UserContext.Provider value={{users:[{name:'abel',email:'abel@mit.edu',password:'secret',balance:100}]}}>
         <div className="container" style={{padding: "20px"}}>
-          <Route path="/components/" exact component={Home} />
-          <Route path="/components/CreateAccount/" component={CreateAccount} />
-          <Route path="/components/login/" component={Login} />
-          <Route path="/components/deposit/" component={Deposit} />
-          <Route path="/components/withdraw/" component={Withdraw} />
-          <Route path="/components/balance/" component={Balance} />
-          <Route path="/components/alldata/" component={AllData} />
+          <Route path="./components/" exact component={Home} />
+          <Route path="./components/" component={CreateAccount} />
+          <Route path="./components/" component={Login} />
+          <Route path="./components/" component={Deposit} />
+          <Route path="./components/" component={Withdraw} />
+          <Route path="./components/" component={Balance} />
+          <Route path="./components/" component={AllData} />
         </div>
       </UserContext.Provider>      
     </HashRouter>
